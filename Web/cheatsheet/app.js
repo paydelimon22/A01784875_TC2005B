6ea819afd66831a80d436252ff1a68a38fb2472a
paydelimon22 <valentina2212.gonzalez@gmail.com>
2024-05-17 10:46:44 -0600

    CSS Cheat Sheet

--- Web/cheatsheet/app.js
+++ Web/cheatsheet/app.js
@@ -11,8 +11,13 @@ const port = 5500;
 
 app.use(express.json());
 
-app.get("/", (req,res)=>{
+app.get("/cheat_sheet", (req,res)=>{
     const file = fs.readFileSync("public/html/index.html", "utf-8");
+    res.status(200).send(file);
+})
+
+app.get("/cheat_sheet_css", (req,res)=>{
+    const file = fs.readFileSync("public/html/css_cheat_sheet.html", "utf-8");
     res.status(200).send(file);
 })
 
